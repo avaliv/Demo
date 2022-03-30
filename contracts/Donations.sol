9f16eb36357aa1de4729709476a2c5ecffd28ca4
avaliv <avaliv@yandex.ru>
2022-03-30 13:36:35 +0700

    Update Donations.sol

--- contracts/Donations.sol
+++ contracts/Donations.sol
@@ -2,33 +2,32 @@
 pragma solidity ^0.8.0;
 
 contract Donations {
-    mapping (address => uint) donators;
+    address[] donators;
+    mapping (address => uint) donat;
     address public owner;
-    address public donatAddress;
     
     constructor() {
         owner = msg.sender;
-        donatAddress = address(this);
     }
 
     function getBalanceDonator(address _addr) public view returns(uint) {
-        return donators[_addr];
+        return donat[_addr];
     }
 
-//    function getDonators( ) public view returns(string memory) {
-//        donators.length;
-//        return donators;
-//    }
+    function getDonators( ) public view returns(address[] memory) {
+        return donators;
+    }
     
     function withdraw(uint _donat, address _addr) public {
-        require(owner == msg.sender && donatAddress.balance > 0, "Rejected");
+        require(owner == msg.sender && address(this).balance > 0, "Rejected ");
         address payable receiver = payable( _addr);
         receiver.transfer( _donat);
     }
     
     receive() external payable {
-        require(msg.value >= 0, "Rejected");
-        donators[msg.sender] += msg.value;
+        require(msg.value > 0, "Rejected");
+        donat[msg.sender] += msg.value;
+        donators.push(msg.sender);
     }
 
 }
